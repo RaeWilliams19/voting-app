bbf66b39b83ca69890d54e8a0bcc8e93ba66745f
Rae Williams <syrae.williams@appliedtechnology.se>
2019-06-10 16:14:17 +0200

    created server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const port = process.env.PORT || 4001;
+const index = require("./routes/index");
+
+const app = express();
+app.use(index);
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    });
+  });
+
